7b88d63f3243faddf41cd113e600079859231aa6
Your Name <you@example.com>
2018-02-27 17:45:47 +0800

    change

--- my-project/routes/index.js
+++ my-project/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var UserModel = require("../model/UserModel");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('register', { title: '注册画面' });
 });
+
+router.post('/api/register',function(req,res){
+	//所有的post参数都被包装到req.body中
+	var username = req.body.username;
+	var psw = req.body.psw;
+	//保存功能 mongodb的调用需要使用mongoose组件
+	var um = new UserModel();
+	um.username = username;
+	um.psw = psw;
+	um.save(function(err){
+		if(err){
+			console.log("注册失败",+err);
+			res.send("注册失败")
+		}else{
+			console.log("注册成功");
+			res.send("注册成功")
+		}
+	})
+})
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
